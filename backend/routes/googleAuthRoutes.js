49c7cc6887fc317c96c1ae95bc38aa8d04b3b86a
Shivansh Garg <shivanshgarg007@gmail.com>
2025-06-01 16:45:01 +0530

    Updated code

--- backend/routes/googleAuthRoutes.js
+++ backend/routes/googleAuthRoutes.js
@@ -4,18 +4,6 @@ const { success, failure } = require("../controllers/googleAuthController");
 
 const router = express.Router();
 
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/auth/failure",
-  }),
-  (req, res) => {
-    res.redirect("http://localhost:5173/");
-  }
-);
-
 router.get("/success", success);
 router.get("/failure", failure);
 
